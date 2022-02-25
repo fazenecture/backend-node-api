43da3a992447d19f70388c9a58e9e803ccc4ced4
Vivek <vivekkumarsingh2002@gmail.com>
2022-02-26 05:02:05 +0530

    first!

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require("dotenv").config();
+const database = require('./utils/database');
+const router = require('./routes/reports-route');
+
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/yogdaan', router);
+
+app.get('*', (req,res) => {
+    res.status(404).send({"Message":"Route Not Found"});
+});
+
+
+app.listen(3000);
