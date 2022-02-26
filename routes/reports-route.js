529356161d05df192b3ad08808765531c8075bf6
Vivek <vivekkumarsingh2002@gmail.com>
2022-02-26 11:18:23 +0530

    -

---
+++ routes/reports-route.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const reportsController = require('../controllers/reports');
+
+const router = express.Router();
+
+router.get('/getCases', reportsController.getCases);
+
+router.post('/createCase' , reportsController.createCases);
+
+router.post('/createUser', reportsController.createUser);
+
+router.get('/getUser/:uUID' , reportsController.getUser);
+
+router.get('/getCaseById/:caseId', reportsController.getCaseById);
+
+router.post('/createOrg', reportsController.createOrg);
+
+router.get('/getOrg/:uUID', reportsController.getOrg);
+
+router.post('/acceptCase', reportsController.acceptCase);
+
+router.get('/getServicesNearYou', reportsController.getServicesNearYou);
+
+router.get('/getMyCases/:uUID', reportsController.getMyCases);
+
+router.post('/deleteCaseById', reportsController.deleteCaseById);
+
+router.post('/editProfile', reportsController.editProfile);
+
+router.post('/welcomeMail', reportsController.welcomeMail);
+
+module.exports = router;
